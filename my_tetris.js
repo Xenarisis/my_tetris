9688d0c8b919c7e91cffed226ee71140545e3fb0
Romain <romainrobert.xen@gmail.com>
2026-01-21 14:37:23 +0100

    Update my_tetris.js

--- my_tetris.js
+++ my_tetris.js
@@ -1,1 +1,17 @@
-// js part later
+// import * as keys from "./keys.js"
+import * as display from "./display.js"
+
+class Game{
+    constructor() {
+        this.Score = 0
+        this.Lines = 0
+        this.Level = 1
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    document.getElementById("start_button").addEventListener("click", () => display.IsGrid.draw())
+}
