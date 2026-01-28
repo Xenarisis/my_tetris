16e2fc7603db8f5eda0862f791fdd37e7abb233c
Romain <romainrobert.xen@gmail.com>
2026-01-28 13:48:16 +0100

    feat: add keys

--- keys.js
+++ keys.js
@@ -4,22 +4,27 @@ document.addEventListener('keydown', (event) => {
     switch(event.key) {
         case 'ArrowLeft':
         case 'q':
+        case 'Q':
             Game.IsGame.moveLeft()
             break
         case 'ArrowRight':
         case 'd':
+        case 'D':
             Game.IsGame.moveRight()
             break
         case 'ArrowDown':
         case 's':
+        case 'S':
             Game.IsGame.softDrop()
             break
         case 'ArrowUp':
         case 'x':
+        case 'X':
             Game.IsGame.clockWiseRotate()
             break
         case 'Control':
         case 'z':
+        case 'Z':
             Game.IsGame.counterClockWiseRotate()
             break
         case ' ':
